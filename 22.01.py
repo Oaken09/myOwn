7e99d69f8550e48a9df8e9f608760bba0ed08efe
Oaken09 <romankoshkin2011@gmail.com>
2025-01-22 11:58:34 +0700

    Add files via upload

---
+++ 22.01.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+df_Score = pd.read_excel('Score.xlsx')
+print('Что нужно вывести?')
+print('1. Строчку')
+print('2. Столбик')
+print('3. Ячейку')
+input1 = int(input('Введите число: '))
+if input1 = 1:
+      input2 = int(input('Введите номер строчки'))
+      print(df.iloc[input2])
+elif input1 = 2:
+      input3 = print('Введите номер столбика')
+      print(df.iloc[input3])
+elif input1 = 3:
+      input4 = int(input('Введите номер строчки'))
+      input5 = int(input('Введите номер строчки'))
+      print(df_Score.iloc[x:y])
